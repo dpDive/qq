b82bfb5d56543160ee96d6d1c61cfcf3c6303bd6
a <52129038+dpDive@users.noreply.github.com>
2019-05-19 11:25:35 +0800

    redirect

---
+++ index.js
@@ -0,0 +1,29 @@
+function openWindow({redirectURL, isRedirect=true, windowNameSpecific}){
+  if(isRedirect){
+    window.name = window.name || 'x'
+  }
+  else if(windowNameSpecific){
+    window.name = windowNameSpecific
+  }
+
+  var {url, windowName, windowFeatures} = {
+    url: redirectURL || 'https://docs.google.com/forms/d/e/1FAIpQLScAKYe1mG8o8Vfxj0khZWWHbMg7upEguqFzAYEESTBROwy8LA/viewform?usp=pp_url&entry.950619120'
+    +'='+window.location.search.replace(/[?][\S\s]+q[=]/).replace(/[&][\S\s]+/),
+
+    windowName: window.name, //_blank //null
+    windowFeatures: undefined,
+  }
+
+  window.open(url, windowName)
+}
+
+openWindow()
+
+
+
+// <a
+//  href="http://www.google.com/"
+//  target="_blank"
+//  onclick="openPopup(); return false;" 
+//  title="This link will create a new window or will re-use an already opened one"
+// >Open Popup</a>
